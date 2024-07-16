19c49f778d52a9b7003c5840d421e8fcf8087c42
Sant-Thiago <thiagovieirab2b@gmail.com>
2024-07-16 18:03:52 -0300

    atualizando conectionMongo

--- backend/database/ConectionMongo.py
+++ backend/database/ConectionMongo.py
@@ -4,10 +4,16 @@ import pymongo.errors
 class ConectionMongo:
     def __init__(self):
         try:
-            url = 'mongodb+srv://Thiago123:Thiago123@tigoscluster.0nmjkid.mongodb.net/?appName=TigosCluster'
+            mongo_host = '172.17.0.2'
+            mongo_port = 27017
+            mongo_user = 'lilUser'
+            mongo_password = 'LilUser123'
+            database_name = 'Web-Scraping'
+            
+            # url = 'mongodb+srv://Thiago123:Thiago123@tigoscluster.0nmjkid.mongodb.net/?appName=TigosCluster'
 
             # Criação do cliente MongoBD
-            self.client = pymongo.MongoClient(url)
+            self.client = pymongo.MongoClient(f'mongodb://{mongo_user}:{mongo_password}@{mongo_host}:{mongo_port}/{database_name}')
             print('[SUCESS] Conexão com MongoDB estabelecida com sucesso!')
         except pymongo.errors.ConnectionFailure as e:
             print(f'[ERROR] Erro ao conectar ao MongoBD: {e}')
